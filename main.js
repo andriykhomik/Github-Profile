d29f148577c35a8501528b8ebfbf257ae23eef4d
andriykhomik <andriykhomik1992@gmail.com>
2021-11-24 19:12:24 +0200

    finished this project by tutorial

--- main.js
+++ main.js
@@ -3,18 +3,14 @@ const form = document.querySelector('#form');
 const search = document.querySelector('#search');
 const card = document.querySelector('.card');
 const main = document.querySelector('#main');
-let repoArray = [];
-let currentUser;
 
-// const username = card.querySelector('.user-info h2');
-// const username = card.querySelector('.user-info h2');
 
 async function getUser(username){
     try {
         const {data} = await axios(APIURL + username);
-        createCard(data);
+        createUserCard(data);
+        getRepos(username);
     } catch (err) {
-        console.log (err);
         if (err.response.status === 404){
             createErrorCard('Not profile wit such username');
         }
@@ -24,16 +20,15 @@ async function getUser(username){
 async function getRepos(username){
     try {
         const {data} = await axios(APIURL + username + '/repos?sort=created');
-        createRepos(data);
+        addReposToCard(data);
     } catch (err) {
-        console.log (err);
-        createErrorCard('Problems fetching repos');
+            createErrorCard('Problem fetching repos ');
     }
 }
 
-function createCard(user){
-    main.innerHTML = '';
-    main.innerHTML = `
+function createUserCard(user){
+
+    const cardHTML = `
         <div class="card">
             <div>
                 <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
@@ -42,59 +37,49 @@ function createCard(user){
                 <h2>${user.name}</h2>
                 <p>${user.bio}</p>
                 <ul>
-                    <li>${user.followers}<strong>Followers </strong></li>
-                    <li>${user.following}<strong>Following </strong></li>
-                    <li>${user.public_repos}<strong>Repos </strong></li>
+                    <li>${user.followers}<strong>Followers</strong></li>
+                    <li>${user.following}<strong>Following</strong></li>
+                    <li>${user.public_repos}<strong>Repos</strong></li>
                 </ul>
-                <div id="repos">
-<!--                    <a href="#" class="repo">Repo 1</a>-->
-<!--                    <a href="#" class="repo">Repo 2</a>-->
-<!--                    <a href="#" class="repo">Repo 3</a>-->
-                </div>
+                <div id="repos"></div>
             </div>
         </div>
+    `;
+
+    main.innerHTML = cardHTML;
+}
+
+function addReposToCard(repos){
+    const reposEl = document.getElementById('repos');
+
+    repos
+        .slice(0, 5)
+        .forEach(repo => {
+        const repoEl = document.createElement('a');
+        repoEl.classList.add('repo');
+        repoEl.href = repo.html_url;
+        repoEl.target = '_blank';
+        repoEl.innerText = repo.name;
+
+        reposEl.appendChild(repoEl);
+    })
+}
+
+function createErrorCard(message){
+    const cardHTML = `
+        <div class="card">
+            <h1>${message}</h1>
+        </div>
     `
+    main.innerHTML = cardHTML;
 }
 
 form.addEventListener('submit', (e)=> {
     e.preventDefault()
-    const user = search.value
+    const user = search.value;
+
     if (user){
         getUser(user);
-        getRepos(user);
-        currentUser = user;
         search.value = '';
-
     }
 })
-
-function createErrorCard(message){
-    main.innerHTML = '';
-    main.innerHTML = `
-        <div class="card">
-           ${message}
-        </div>
-    `
-}
-
-function createRepos(repos){
-    let i = 0;
-    repos.forEach(repo => {
-        const reposTag = document.querySelector('#repos');
-        if (i < 5){
-            const a = document.createElement('a');
-            a.href = repo.html_url;
-            a.classList.add('repo');
-            a.target = '_blank';
-            a.innerText = repo.name;
-            reposTag.appendChild(a);
-        }
-        i++
-    })
-}
-
-
-
-
-
-
